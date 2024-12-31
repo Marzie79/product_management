ee65ebba5c8ca7d071eb17acf3d6b732dfb316e5
Marzie79 <marzie.7900@gmail.com>
2025-01-01 00:27:25 +0330

    refactor: enhance authentications url

--- product_management/urls.py
+++ product_management/urls.py
@@ -5,7 +5,7 @@ from django.urls import (path, include)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/accounts/', include('authentications.urls')),
+    path('api/v1/auth/', include('authentications.urls')),
     path('api/v1/products/', include('products.urls')),
 ]
 
